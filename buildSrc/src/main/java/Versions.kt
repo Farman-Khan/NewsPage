f42a0a9c6cc0cc2d287ee7f04c02907ed4220def
Farman Khan <farmankhan@farmans-MacBook-Pro.local>
2023-11-21 13:42:59 +0800

    added kotlin dsl with buildSrc

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val coreKtx = "1.9.0"
+    const val lifecycleRuntimeKtx = "2.6.2"
+    const val activityCompose = "1.8.0"
+    const val composeBom = "2023.03.00"
+    const val junit = "4.13.2"
+    const val junitExt = "1.1.5"
+    const val espresso = "3.5.1"
+}
